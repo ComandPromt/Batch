d4b173c5c4c900afc2febac0cc1a36bace1225b8
ComandPromt <vayase_a_ferrol@hotmail.com>
2017-10-02 18:17:10 +0200

    Add files via upload

---
+++ borrartemp.bat
@@ -0,0 +1,9 @@
+@echo off
+cd "C:\Users\ComandPromt\AppData\Local\Microsoft\Windows\Temporary Internet Files"
+DEL *.*  /S /F /Q /A R H S A
+del /f /s /q "%USERPROFILE%\AppData\Local\Temp\*.*"
+rmdir /s /q "C:\Users\%username%\AppData\Local\Temp"
+mkdir "C:\Users\%username%\AppData\Local\Temp" 
+cd C:\Windows\Temp\
+DEL *.*  /S /F /Q /A R H S A
+exit 0
