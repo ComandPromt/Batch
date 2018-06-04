01f0c9e3ef1c82a2e620011df6a8b634eda7754f
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-06-04 09:03:20 +0200

    Update plantilla menu.bat

--- plantilla menu.bat
+++ plantilla menu.bat
@@ -1,35 +1,24 @@
 @echo off
 
-
 color 2e
 
 title Bailarines
 
-
 :ini
 
-
 cls
-
 
 echo --------------------------------------------------------------------------------
 
 echo.
 
 echo                 "##\      ##\                               "
-
 echo                 "###\    ### |                              "
-
 echo                 "####\  #### | ######\  #######\  ##\   ##\ "
-
 echo                 "##\##\## ## |##  __##\ ##  __##\ ## |  ## |"
-
 echo                 "## \###  ## |######## |## |  ## |## |  ## |"
-
 echo                 "## |\#  /## |##   ____|## |  ## |## |  ## |"
-
 echo                 "## | \_/ ## |\#######\ ## |  ## |\######  |"
-
 echo                 "\__|     \__| \_______|\__|  \__| \______/ "
 
 echo.
@@ -44,20 +33,13 @@ echo.
 
 set /p inicio="32 o 64 bits?: "
 
-
 if %inicio%==64 (goto 64) else if %inicio%==32 (goto 32) else (goto ini)
-
-
 
 :64
 
-
 start explorer.exe "C:\Program Files (x86)\Microsoft Plus! Digital Media Edition\Dancer\Dancers"
 
-
 exit
-
-
 
 :32
 
