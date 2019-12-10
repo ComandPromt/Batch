8e567043ec0d8224dc66bc6f8aa0df2c11a6143b
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-12-10 16:20:45 +0100

    Update borrartemp.bat

--- borrartemp.bat
+++ borrartemp.bat
@@ -1,9 +1,9 @@
 @echo off
-cd "C:\Users\%username%\AppData\Local\Microsoft\Windows\Temporary Internet Files"
-DEL *.*  /S /F /Q /A R H S A
-del /f /s /q "%USERPROFILE%\AppData\Local\Temp\*.*"
+del /f /s /q "C:\Users\%username%\AppData\Local\Temp\*.*"
 rmdir /s /q "C:\Users\%username%\AppData\Local\Temp"
 mkdir "C:\Users\%username%\AppData\Local\Temp" 
-cd C:\Windows\Temp\
-DEL *.*  /S /F /Q /A R H S A
-exit 0
+del /f /s /q "C:\Windows\Temp\*.*"
+del /f /s /q "C:\Documents and Settings\%username%\Configuración local\Temp\*.*" 
+exit
+
+
