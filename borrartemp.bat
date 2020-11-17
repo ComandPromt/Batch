bf19f705583f4f777fc18b26e06a99ff512c902e
ComandPromt <vayase_a_ferrol@hotmail.com>
2020-11-17 19:42:33 +0100

    Update borrartemp.bat

--- borrartemp.bat
+++ borrartemp.bat
@@ -3,7 +3,4 @@ del /f /s /q "C:\Users\%username%\AppData\Local\Temp\*.*"
 rmdir /s /q "C:\Users\%username%\AppData\Local\Temp"
 mkdir "C:\Users\%username%\AppData\Local\Temp" 
 del /f /s /q "C:\Windows\Temp\*.*"
-del /f /s /q "C:\Documents and Settings\%username%\Configuración local\Temp\*.*" 
 exit
-
-
